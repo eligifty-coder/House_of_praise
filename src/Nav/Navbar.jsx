3d7be9566f0ae72e7cadc0870e8b290c0f15db7f
gift-minwo <gift@minwo.co>
2022-05-29 21:27:43 +0100

    welcome section for small screen complete

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useState} from 'react'
 import classes from './Navbar.module.css'
 import Logo from '../images/logo.png'
 import Hamburger from '../images/hamburger.png'
@@ -8,13 +8,34 @@ import Image3  from '../images/image3.jpeg'
 import ArrowLeft from '../images/arrow-left.svg'
 import ArrowRight from '../images/arrow-right.svg'
 const Navbar = () => {
+   const [count, setCount] = useState(0)
+   const [buttonLeft, setButtonLeft] = useState('')
+   const [buttonRight, setButtonRight] = useState('')
    const imageArray = [Image, Image2, Image3, Image, Image2]
-   let displayedImage = imageArray[0]
+
+   let displayedImage = imageArray[count]
    const handleImgLoop = () => {
-      
+      setButtonLeft(classes.red)
+      setButtonRight('')
+      setCount(prev=>prev+1)
+      if(count >= imageArray.length-1){
+         setCount(imageArray.length - 1)
+         return
+      }
    }
+   const handleLoopBack = () => {
+      setButtonRight(classes.red)
+      setButtonLeft('')
+      setCount(prev=>prev-1)
+      if(count <=0){
+         setCount(0)
+         return
+      }
+   }
+
+
    return (
-      <section className={classes.section}>
+      <section className={classes.section} id='welcome'>
          <header className={classes.header}>
             <div className={classes.start}>
                <p>JOIN US THIS SUNDAY AT 9:00AM IN PERSON OR VIA
@@ -35,21 +56,21 @@ const Navbar = () => {
             <div className={classes.carousel}>
                <img src={displayedImage} />
                <div className={classes.controls}>
-                  <button className={classes.left} >
+                  <button className={buttonRight} onClick={handleLoopBack} >
                      <img src={ArrowLeft} alt="arrow-left" />
                   </button>
-                  <button className={classes.right} onClick={handleImgLoop} >
+                  <button className={ buttonLeft} onClick={handleImgLoop} >
                      <img src={ArrowRight} alt="arrow-right" />
                   </button>
                </div>
          </div>
-            <h2>
+            <h2 className={classes.title}>
                Welcome to CGMI
-               <span>House of Praise</span>
+               <span> House of Praise</span>
             </h2>
             <div className={classes.actions}>
-               <button>Upcoming Events </button>
-               <button>Service Days </button>
+               <a href='#about'>Upcoming Events </a>
+               <a href='#about' className={classes.transparent}>Service Days </a>
             </div>
          </div>
       </section>

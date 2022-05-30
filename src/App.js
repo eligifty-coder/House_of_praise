5c5499490c3c5d7b745fbd6f623a4da59140b8a1
gift-minwo <gift@minwo.co>
2022-05-30 16:27:42 +0100

    section improve complete

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css'
 import Nav from './components/Nav/Navbar';
 import About from './components/about/About';
 import Connect from './components/connect/Connect';
-// import Improve from './components/improve'
+import Improve from './components/improve/Improve'
 function App() {
   return (
     <div className="App">
       <Nav />
       <About />
-      <Connect/>
+      <Connect />
+      <Improve/>
     </div>
   );
 }

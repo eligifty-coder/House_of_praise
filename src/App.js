03020a5243bff6de304657d37afa438425d9abd9
gift-minwo <gift@minwo.co>
2022-06-05 11:48:05 +0100

    complete service

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Nav from './components/Nav/Navbar';
 import About from './components/about/About';
 import Connect from './components/connect/Connect';
 import Improve from './components/improve/Improve'
+import Program from './components/programs/Program'
 function App() {
   return (
     <div className="App">
       <Nav />
       <About />
       <Connect />
-      <Improve/>
+      <Improve />
+      <Program />
     </div>
   );
 }
